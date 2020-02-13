a687c7c5d9cbd4e611a712a02c477133f21e553b
Lanczi Dominik <nhcmon@gmail.com>
2020-02-13 15:04:20 +0100

    Added how to use info

--- hexo-css-merge-script.js
+++ hexo-css-merge-script.js
@@ -1,8 +1,8 @@
 var fs = require('hexo-fs');
 
-var contents = fs.readFileSync('themes\\landscape\\source\\css\\global.css');
-var contentstwo = fs.readFileSync('themes\\landscape\\source\\css\\navbar.css');
-var allcontent = contents + contentstwo;
+var contents = fs.readFileSync('themes\\/*your-theme-name-here*/\\source\\css\\/*css-file-name-here*/.css'); // Fill out the line like shown at the example
+var contents2 = fs.readFileSync('themes\\landscape\\source\\css\\navbar.css'); // Example
+var allcontent = contents + contents2; // Add the files together in the order you would like to have the codes
 
 var CleanCSS = require('clean-css');
 var input = allcontent;
@@ -13,7 +13,7 @@ hexo.extend.generator.register('css-merge', function (locals) {
 
     return {
 
-        path: '/css/main.css',
+        path: '/css/main.css', // Merged and minified css file's path and name
         data: function(){
             return output.styles;
         }
